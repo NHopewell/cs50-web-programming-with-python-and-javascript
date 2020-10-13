e5251d99cfe372960a1605f062610973f0fca5e3
Nick Hopewell <nicholashopewell@gmail.com>
2020-10-12 22:07:15 -0400

    adds markdown to html conversion

--- week_3_Django/wiki/encyclopedia/views.py
+++ week_3_Django/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown, markdown
 
 from . import util
 
@@ -12,11 +13,17 @@ def page(request, title):
     """
     Render html page based off of the title url.
     """
+
+    page = util.get_entry(title)
+
     if not page:
         return render(request, "encyclopedia/404.html")
+
+    converter = Markdown()
+    html_page = converter.convert(page)
     return render(request, "encyclopedia/page.html", {
         "title": title,
-        "page": util.get_entry(title)
+        "page": html_page
     })
 
 
