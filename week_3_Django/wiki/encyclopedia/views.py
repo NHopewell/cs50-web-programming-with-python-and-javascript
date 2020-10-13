90d8833d3a671827945f76a4f5ed276d426a4f51
Nick Hopewell <nicholashopewell@gmail.com>
2020-10-13 09:06:01 -0400

    adds search

--- week_3_Django/wiki/encyclopedia/views.py
+++ week_3_Django/wiki/encyclopedia/views.py
@@ -1,8 +1,11 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from markdown2 import Markdown, markdown
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -13,17 +16,37 @@ def page(request, title):
     """
     Render html page based off of the title url.
     """
+    if request.method == "POST":
+        query = request.POST.get("q")
+        if query in util.list_entries():
+            page = util.get_entry(title)
+            converter = Markdown()
+            html_page = converter.convert(page)
 
-    page = util.get_entry(title)
+            return HttpResponseRedirect(reverse("page"), {
+                "title": query,
+                "page": html_page
+            })
+        else:
+            matches = []
+            for entry in util.list_entries():
+                if query.lower() in entry.lower():
+                    matches.append(entry)
 
-    if not page:
-        return render(request, "encyclopedia/404.html")
+            return HttpResponseRedirect(reverse("index"), {
+                "entries": matches
+            })
+    else:
+        page = util.get_entry(title)
 
-    converter = Markdown()
-    html_page = converter.convert(page)
-    return render(request, "encyclopedia/page.html", {
-        "title": title,
-        "page": html_page
-    })
+        if not page:
+            return render(request, "encyclopedia/404.html")
+
+        converter = Markdown()
+        html_page = converter.convert(page)
+        return render(request, "encyclopedia/page.html", {
+            "title": title,
+            "page": html_page
+        })
 
 
