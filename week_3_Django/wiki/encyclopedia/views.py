993ec1a91b95ac4368fc99201b0a7e4665f1d846
Nick Hopewell <nicholashopewell@gmail.com>
2020-10-17 09:06:46 -0400

    fixes page search

--- week_3_Django/wiki/encyclopedia/views.py
+++ week_3_Django/wiki/encyclopedia/views.py
@@ -8,22 +8,15 @@ from markdown2 import Markdown, markdown
 from . import util
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-def page(request, title):
-    """
-    Render html page based off of the title url.
-    """
     if request.method == "POST":
         query = request.POST.get("q")
         if query in util.list_entries():
-            page = util.get_entry(title)
+
+            page = util.get_entry(query)
             converter = Markdown()
             html_page = converter.convert(page)
 
-            return HttpResponseRedirect(reverse("page"), {
+            return render(request, "encyclopedia/page.html", {
                 "title": query,
                 "page": html_page
             })
@@ -33,20 +26,29 @@ def page(request, title):
                 if query.lower() in entry.lower():
                     matches.append(entry)
 
-            return HttpResponseRedirect(reverse("index"), {
+            return render(request, "encyclopedia/index.html", {
                 "entries": matches
             })
     else:
-        page = util.get_entry(title)
 
-        if not page:
-            return render(request, "encyclopedia/404.html")
-
-        converter = Markdown()
-        html_page = converter.convert(page)
-        return render(request, "encyclopedia/page.html", {
-            "title": title,
-            "page": html_page
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
         })
+
+def page(request, title):
+    """
+    Render html page based off of the title url.
+    """
+    page = util.get_entry(title)
+
+    if not page:
+        return render(request, "encyclopedia/404.html")
+
+    converter = Markdown()
+    html_page = converter.convert(page)
+    return render(request, "encyclopedia/page.html", {
+        "title": title,
+        "page": html_page
+    })
 
 
