6d2fbd1652f21c98355f6262be49fee497781e2a
Nick Hopewell <nicholashopewell@gmail.com>
2020-10-20 08:50:59 -0400

    adds auctions models

--- week_4_SQL_Models_Migrations/commerce/auctions/models.py
+++ week_4_SQL_Models_Migrations/commerce/auctions/models.py
@@ -1,6 +1,41 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from  django.utils import timezone
 
 
 class User(AbstractUser):
-    pass
+    
+    def __str__(self):
+        return f"id: {self.id}, name: {self.first_name} {self.last_name}""
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    list_price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(default='default.png', upload_to='listing_pics')
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}, posted by: {self.owner}."
+
+
+class Bid(models.Model):
+    bid = models.DecimalField(max_digits=8, decimal_places=2)
+    bidder = models.ForeignKey(User, no_delete=models.CASCADE)
+    bid_date = date_posted = models.DateTimeField(default=timezone.now)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bid: {self.bid}, Bidder: {self.bidder}"
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.author} on post titled: '{self.listing.title}'."
