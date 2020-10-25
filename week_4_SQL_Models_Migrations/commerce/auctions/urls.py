8873510fd306fa09d0dc2b6d2e0dc97e2f1a5286
Nick Hopewell <nicholashopewell@gmail.com>
2020-10-25 18:25:30 -0400

    fix urls

--- week_4_SQL_Models_Migrations/commerce/auctions/urls.py
+++ week_4_SQL_Models_Migrations/commerce/auctions/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("listing-<int:listing_id>", views.listing, name="listing"),
-    path("create_listing", views.create_listing, name="create_listing"),
-    path("listing-<int:listing_id>/delete", views.delete_listing, name="delete_listing"),
-    path("watchlist", views.watchlist, name="watchlist"),
-    path("categories", views.categories, name="categories"),
-    path("categories/<str:category>", views.category, name="category")
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("listing-<int:listing_id>/", views.listing, name="listing"),
+    path("create_listing/", views.create_listing, name="create_listing"),
+    path("listing-<int:listing_id>/delete/", views.delete_listing, name="delete_listing"),
+    path("watchlist/", views.watchlist, name="watchlist"),
+    path("categories/", views.categories, name="categories"),
+    path("categories/<str:category>/", views.category, name="category")
     
 ]
