7b1deac4e42acfee682216c819b4f1f18550ccc9
Nick Hopewell <nicholashopewell@gmail.com>
2020-11-02 19:08:33 -0500

    update doc strings

--- week_4_SQL_Models_Migrations/commerce/auctions/helpers.py
+++ week_4_SQL_Models_Migrations/commerce/auctions/helpers.py
@@ -4,11 +4,12 @@ from math import modf
 from .models import User, Listing, Watchlist, CATEGORY_CHOICES
 
 def silver_and_gold(bid):
+    """given a float, return fractional part as silver, rest as gold"""
     s, g = modf(bid)
     return (int(s*100), int(g))
 
 def get_diff_days(date):
-
+    """return diff between two datetimes as days"""
     date_format = "%Y-%m-%d"
     date_one = datetime.datetime.strptime(str(date).split()[0], date_format)
     date_two = datetime.datetime.strptime(str(datetime.date.today()), date_format)
@@ -19,25 +20,23 @@ def get_diff_days(date):
 
 
 def map_category(category, choices=CATEGORY_CHOICES):
-
-   for (k,v) in choices:
-       if k == category:
-           return v
+    """map key to category val"""
+    for (k,v) in choices:
+        if k == category:
+            return v
 
 def all_categories(Listing):
-
+    """map categories to associated listings as dict"""
     all_categories = ['Art & Collectibles', 'Clothing', 
         'Electronics', 'Health & Beauty', 'Home & Yard', 
         'Jewellery', 'Sporting Goods']
-
     category_listings = [Listing.objects.filter(category=cat) for cat in all_categories]
 
-    categories = dict(zip(all_categories, category_listings))
+    return dict(zip(all_categories, category_listings))
 
-    return categories
 
 def get_listings_by_category(Listing, category, choices=CATEGORY_CHOICES):
-
+    """return all active listings under a category"""
     for (k,v) in choices:
         if v == category: 
             listings = Listing.objects.filter(category=k)
